d17053e36b34418aee2ebb2204a077630337b86c
ShahlaAliyeva <raufa5740@gmail.com>
2022-02-26 00:47:12 +0400

    page is ready

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,20 @@
 function random(min, max) {
-    return Math.floor(Math.random()*(max - min + 1))+min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var targetScore = random(15, 150);
+// var targetScore = random(15, 150);
+// var blue = $('.blue').val(random(1, 15));
+// var green = $('.green').val(random(1, 15));
+// var red = $('.red').val(random(1, 15));
+// var yellow = $('.yellow').val(random(1, 15));
 
-$(document).ready(function() {
-    $(".targetScore").text(targetScore);
+$(document).ready(function () {
+    function beginGame() {
+        var targetScore = random(15, 150);
+        var blue = $('.blue').val(random(1, 15));
+        var green = $('.green').val(random(1, 15));
+        var red = $('.red').val(random(1, 15));
+        var yellow = $('.yellow').val(random(1, 15));
+        $(".targetScore").text(targetScore);
+    }
 })
-
