ee319efb336e76d42bfce72eebc72d2109e1b826
ShahlaAliyeva <raufa5740@gmail.com>
2022-02-26 01:09:49 +0400

    page is ready

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,25 @@ var targetScore = 0;
 var winCount = 0;
 var lossCount = 0;
 
+var crystals = {
+    blue: {
+        name: 'Blue',
+        value: 0
+    },
+    green: {
+        name: 'Green',
+        value: 0
+    },
+    red: {
+        name: 'Red',
+        value: 0
+    },
+    yellow: {
+        name: 'Yellow'
+        
+    }
+}
+
 // var targetScore = random(15, 150);
 // var blue = $('.blue').val(random(1, 15));
 // var green = $('.green').val(random(1, 15));
@@ -21,6 +40,14 @@ $(document).ready(function () {
         var green = $('.green').val(random(1, 15));
         var red = $('.red').val(random(1, 15));
         var yellow = $('.yellow').val(random(1, 15));
-        $(".targetScore").text(targetScore);
+        $('.targetScore').text(targetScore);
+        $('.userScore').text(userScore);
+
+        console.log('*******************************');
+        console.log('Target Score: ', targetScore);
+        console.log('Blue val: ', blue, '\n Green val: ', green, '\n Red val: ', red, '\n Yellow val: ', yellow)
+        console.log('*******************************');
     }
+
+    beginGame()
 })
